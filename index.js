5821b2fd253b95a4f743635d2dc6bd1b50b383d6
ekingan <ekingan@gmail.com>
2017-09-06 16:04:01 -0700

    Add routes and services

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+require('./services/passport')
 const express = require('express'),
 	app = express()
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' })
-})
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT)
